68965b62e7cbea88871462c4fd37a0c67c45073d
John Roussos <jroussosdev@gmail.com>
2025-07-20 19:19:37 +0300

    ADD: swiper slider on demo html as proof of concept

--- demo/index.js
+++ demo/index.js
@@ -1,21 +1,44 @@
 import ColorChecker from '../dist/contrast-checker.js';
 
-const checker = new ColorChecker({
-    child: '#lorem',
+const checker = new ColorChecker()
+
+function handlePaginationBulletColors (swiper) {
+    const bulletsContainer = swiper.pagination.el
+
+    checker.updateOptions({
+        child: bulletsContainer,
+        parent: swiper.slides[swiper.activeIndex].querySelector('img')
+    })
+
+    const color = checker.compare([
+        '#ffffff', 
+        '#000000'
+    ])
+    
+    document.documentElement.style.setProperty('--swiper-pagination-bullet-inactive-opacity', 0.5)
+    document.documentElement.style.setProperty('--swiper-pagination-bullet-inactive-color', color)
+}
+
+function handleSwiperSlideTextInit (swiper) {
+    swiper.slides.forEach(slide => {
+        const child = slide.querySelector('span')
+        
+        child.style.color = checker.updateOptions({child}).compare([
+            '#d23d11', 
+            '#7f99ab', 
+            '#9cdbb9'
+        ])
+    })
+
+    handlePaginationBulletColors(swiper)
+}
+
+window.swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    on: {
+        init: handleSwiperSlideTextInit,
+        slideChangeTransitionEnd: handlePaginationBulletColors,
+    },
 })
-
-document.querySelector('#lorem').style.color = checker.compare([
-    '#ffffff', 
-    '#f00fff', 
-    '#02004b'
-])
-
-checker.updateOptions({
-    child: '#lorem2'
-})
-
-document.querySelector('#lorem2').style.color = checker.compare([
-    '#ffffff', 
-    '#f00fff', 
-    '#02004b'
-])
